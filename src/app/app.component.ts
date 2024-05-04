bf65b7a8c1a7abf782906cd3bcace69034a095ca
tigersf2 <lellahemhamdi@gmail.com>
2024-05-04 20:18:07 +0200

    add redirect login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(authService: AuthService, router: Router) {
+    if (authService.isLoggedIn()) {
+      router.navigate(['dashboard']);
+    }
+  }
 }

3e082875abace9bca1e5e5fcc4067b0b22af8632
tigersf2 <lellahemhamdi@gmail.com>
2024-05-04 19:27:44 +0200

    update label

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,14 +10,14 @@ import { EditTacheComponent } from './component/edit-tache/edit-tache.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'new-list', component: NewListComponent },
+  { path: 'new-list', component: NewListComponent, data:{requiresLogin: true} },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'edit-list/:listeId', component: EditListComponent },
-  { path: 'liste/:listeId/edit-task/:tacheId', component: EditTacheComponent },
+  { path: 'edit-list/:listeId', component: EditListComponent, data:{requiresLogin: true} },
+  { path: 'liste/:listeId/edit-task/:tacheId', component: EditTacheComponent, data:{requiresLogin: true} },
   { path: 'home', component: HomeComponent },
-  { path: 'liste/:listeId', component: HomeComponent },
-  { path: 'liste/:listeId/new-task', component: NewTaskComponent },
+  { path: 'liste/:listeId', component: HomeComponent, data:{requiresLogin: true} },
+  { path: 'liste/:listeId/new-task', component: NewTaskComponent, data:{requiresLogin: true} },
 ];
 
 @NgModule({

836c58249f25cd6275a479de040c0c49e3d81bbe
tigersf2 <lellahemhamdi@gmail.com>
2024-05-04 20:21:44 +0200

    fix issue call

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,18 +7,17 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { EditListComponent } from './component/edit-list/edit-list.component';
 import { EditTacheComponent } from './component/edit-tache/edit-tache.component';
-import { LoginActivate } from './services/canActivate.service';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'new-list', component: NewListComponent, canActivate:[LoginActivate] },
+  { path: 'new-list', component: NewListComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'edit-list/:listeId', component: EditListComponent, canActivate:[LoginActivate] },
-  { path: 'liste/:listeId/edit-task/:tacheId', component: EditTacheComponent, canActivate:[LoginActivate] },
+  { path: 'edit-list/:listeId', component: EditListComponent },
+  { path: 'liste/:listeId/edit-task/:tacheId', component: EditTacheComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'liste/:listeId', component: HomeComponent, canActivate:[LoginActivate] },
-  { path: 'liste/:listeId/new-task', component: NewTaskComponent, canActivate:[LoginActivate] },
+  { path: 'liste/:listeId', component: HomeComponent },
+  { path: 'liste/:listeId/new-task', component: NewTaskComponent },
 ];
 
 @NgModule({
